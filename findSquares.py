5576014c7748ee4e1280403322cad2dcb87b66fa
bd2720 <brendandeneen9@gmail.com>
2025-09-06 00:47:26 -0400

    enable running from cmdline

--- findSquares.py
+++ findSquares.py
@@ -6,98 +6,32 @@
 # L I T
 
 from prefixTree import PrefixTree 
+import sys
 
-N = 6
-IS_COMMON = False
-DICTIONARY = 'webster'
-DIRECTORY = 'words'
-WORD_FILE = f"./{DIRECTORY}/words-{DICTIONARY}{'-common' if IS_COMMON else ''}-{N}.txt"
+DIRECTORY = 'words' # fixed directory name where word files are read from
+VALID_WORD_SETS = {"collins", "webster", "webster-common"}
+DEFAULT_WORD_SET = "webster-common"
 
-def readWords():
+# read words in from the given filename, ensure uppercase
+def readWords(filename):
   words = []
-  with open(WORD_FILE, "r") as word_file:
+  with open(filename, "r") as word_file:
     for line in word_file:
       words.append(line.strip().upper())
   return words
 
-def findSquares_unique_bf(words, limit=100):
-  wordSet = set(words)
-  wordSquares = []
-  # choose first word
-  for w1 in words:
-    #print(f"w1:{w1}")
-    # choose next word
-    for w2 in words:
-      if w2 == w1:
-        continue
-      # choose third word
-      for w3 in words:
-        if w3 == w2 or w3 == w1:
-          continue
-        # check that verticals are words
-        valid = True
-        vertWords = []
-        for i in range(3):
-          vert = w1[i] + w2[i] + w3[i]
-          if vert not in wordSet or vert in [w1, w2, w3] or vert in vertWords:
-            valid = False
-            break
-          vertWords.append(vert)
-        if valid:
-          wordSquares.append([w1, w2, w3])
-          if len(wordSquares) >= limit:
-            return wordSquares
-          
-          #print(f"Found: {wordSquares[-1]}")
-  return wordSquares
-
-def findSquares_prefixtree_3(words):
-  squares = []
-  # Choose words in columns, using prefix tree
-  # 1. create prefix tree
-  pftree = PrefixTree(words, 3)
-  # 2. iterate through all starting words
-  for word1 in words:
-    # 3. Now our next starting letter is fixed. 
-    # Iterate through all words that start with the second letter of the first word
-    words2 = pftree.startsWith([word1[1]])
-    for word2 in words2:
-      # 4. Now our next 2 letters are fixed.
-      # Can make word squares with all valid words that start with (word1[2]+word2[2])
-      words3 = pftree.startsWith([word1[2], word2[2]])
-      if len(words3) > 0:
-        # record new squares
-        squares += [[word1, word2, word3] for word3 in words3]
-  # finally, return all the squares we found
-  return squares
-
-# call with partialSquares = [[w] for w in words]
-def findSquares_prefixtree_r(ptree, depth, n, partialSquares):
-  print(f"Depth: {depth}")
-  if depth >= n or not partialSquares:
-    return partialSquares
-  # for each partial square: calculate
-  newPartialSquares = []
-  for psquare in partialSquares:
-    # construct current prefix
-    prefix = [psquare[i][depth] for i in range(depth)]
-    # find list of words that begin with the prefix
-    candidateWords = ptree.startsWith(prefix)
-    # construct new partialSquares by pairing each candidate word with current partial square
-    newPartialSquares += [psquare + [cword] for cword in candidateWords]
-  return findSquares_prefixtree_r(ptree, depth+1, n, newPartialSquares)
-
 # Find symmetrical word squares using a prefix tree (non-recursive)
-def findSquares_prefixtree(ptree, words, n, startingSquares):
+def findSquares_prefixtree(ptree, words, n, startingSquares=None, shouldPrint=False):
   # build squares starting with each word
   partialSquares = startingSquares if startingSquares else [[w] for w in words] 
   # add on n-1 new words to each square, or remove it
   for depth in range(1, n):
-    print(f"{len(partialSquares)} partial squares of length {depth}...")
+    if shouldPrint:
+      print(f"{len(partialSquares)} partial squares of length {depth}...")
     if not partialSquares:
       break
     # Just for fun, see how close we got
-    if len(partialSquares) <= 10:
+    if shouldPrint and len(partialSquares) <= 10:
       for psquare in partialSquares:
         print(formatSquare(psquare))
     newPartialSquares = []
@@ -113,17 +47,55 @@ def findSquares_prefixtree(ptree, words, n, startingSquares):
   # return all squares found
   return partialSquares
 
-
+# print the word square with the letters spaced out
 def formatSquare(wordSquare):
-  s = ""
-  for horizontalWord in wordSquare:
-    s += " ".join(horizontalWord) + "\n"
-  return s
+  return "\n".join(" ".join(word) for word in wordSquare)
 
+# Usage:
+#   $ python ./findSquares.py <WORD_SET> <N*> <STARTING_WORD>
+#   (* == required)
+#   (can replace optional params with _)
+def main():
+  # try to read WORD_SET
+  try:
+    WORD_SET = sys.argv[1]
+    if WORD_SET not in VALID_WORD_SETS:
+      raise
+  except:
+    WORD_SET = DEFAULT_WORD_SET
+    print(f"Warning: WORD_SET is invalid, defaulting to {DEFAULT_WORD_SET}...")
+  # try to read N
+  try:
+    N = int(sys.argv[2])
+  except:
+    print("Error: N is invalid.")
+    return
+  # assemble word file name
+  WORD_FILE = f"./{DIRECTORY}/words-{WORD_SET}-{N}.txt"
+  # read in list of N-length words
+  words = readWords(WORD_FILE)
+  if not words:
+    print("Error: Words were not read successfully.")
+    return
+  # try to read starting word
+  STARTING_WORD = None
+  try:
+    STARTING_WORD = sys.argv[3].upper()
+  except:
+    pass
+  if STARTING_WORD:
+    if len(STARTING_WORD) != N:
+      print("Error: STARTING_WORD is not a valid length.")
+      return
+    elif STARTING_WORD not in words:
+      print("Error: STARTING_WORD is not a valid word.")
+      return
+  # assemble PrefixTree
+  pftree = PrefixTree(words, N)
+  # find all symmetrical word squares of size N
+  wordSquares = findSquares_prefixtree(pftree, words, N, startingSquares=([[STARTING_WORD]] if STARTING_WORD else None), shouldPrint=True)
+  # print size of results
+  print(f"{len(wordSquares)} word squares of size {N} found" + (f" starting with {STARTING_WORD}!" if STARTING_WORD else "!"))
 
-words = readWords()
-pftree = PrefixTree(words, N)
-# find all symmetrical word squares of size N
-#wordSquares = findSquares_prefixtree(pftree, words, N)
-wordSquares = findSquares_prefixtree(pftree, words, N, startingSquares=[[w] for w in words if w[0] == 'A'])
-print(f"{len(wordSquares)} word squares found!")
+if __name__ == "__main__":
+  main()
